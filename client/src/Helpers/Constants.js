5ef9abf3060977a5e0702633967e7a02b0f4796b
Filo13199 <filo.ict@gmail.com>
2022-04-16 14:10:40 +0200

    Update Constants.js

--- client/src/Helpers/Constants.js
+++ client/src/Helpers/Constants.js
@@ -1,6 +1,6 @@
 var tmp;
  if (process.env.NODE_ENV === "production"){
-  tmp = "https://www.animalsrepublic.org/api/";
+  tmp = "https://nodogoro-ecommerce.herokuapp.com/api/";
 }
 else{
   //tmp = 'http://192.168.1.12:3000/api/';
