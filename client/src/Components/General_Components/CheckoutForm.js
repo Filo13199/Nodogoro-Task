bf87a65d1f8c9d0ff2b4f9873bc99b9e06a99861
Filo13199 <filo.ict@gmail.com>
2022-04-17 11:21:47 +0200

    Update CheckoutForm.js

--- client/src/Components/General_Components/CheckoutForm.js
+++ client/src/Components/General_Components/CheckoutForm.js
@@ -56,7 +56,7 @@ export default function CheckoutForm(props) {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000/checkout",
+        return_url: "https://nodogoro-ecommerce.herokuapp.com/checkout",
       },
     });
 
